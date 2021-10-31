814a40f61d4281297d6ff1de2e83944a05639ead
Nicolas Benko <nicolasbenko@yahoo.com.ar>
2021-10-31 02:57:32 -0300

    Componentes

---
+++ src/Components/ItemListContainer.js
@@ -0,0 +1,7 @@
+const ItemListContainer = ({msj}) => {
+    return (
+        <span>El mensaje es:{msj}</span>
+    )
+}
+
+export default ItemListContainer
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import Menu from "./Components/NavBar"
+
+
+const Header = () =>{
+
+ return(
+     <header>
+       <p>Como estas</p>
+       <Menu/>
+     </header>
+ )
+
+} 
+
+export default Header
