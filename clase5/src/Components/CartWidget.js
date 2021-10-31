195170324ffb2b396acab8d657216ce7fc68a69b
Nicolas Benko <nicolasbenko@yahoo.com.ar>
2021-10-31 17:12:35 -0300

    Desafio 5.Componentes II

--- clase5/src/Components/CartWidget.js
+++
@@ -1,9 +0,0 @@
-const CartWidget = () => {
-    return (
-        <div>
-            <p className="material-icons">shopping_cart</p>
-        </div>
-    )
-}
-
-export default CartWidget
--- clase5/src/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () =>{
-    return(
-        <footer>
-            Soy un maldito Footer
-        </footer>
-    );
-}
-
-export default Footer
